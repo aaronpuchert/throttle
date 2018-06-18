b164535c6cad07609448ecaab9edaaafdc19142f
Aaron Puchert <aaronpuchert@alice-dsl.net>
2018-06-19 01:38:25 +0200

    Use sigtimedwait instead of signal handler
    That's way more straightforward than sleep()-ing and setting some flag
    in the signal handler. We block the signals instead and wait for them
    (the same amount of time we slept before), which makes us immediately
    terminate if we receive one.

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,6 @@
 static const int wait = 3;
 
 /**
- * Handle termination signals.
- */
-bool term = false;
-void sighandle(int sig)
-{
-	term = true;
-}
-
-/**
  * Main function.
  */
 int main(int argc, char **argv)
@@ -26,28 +17,24 @@ int main(int argc, char **argv)
 	config_fn = (argc > 1) ? argv[1] : "throttle.conf";
 	pipe_fn = (argc > 2) ? argv[2] : "pipe";
 
-	// Install signal handler
-	struct sigaction sa;
-	sa.sa_handler = sighandle;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
+	// Block SIGINT, SIGQUIT, SIGTERM - we're going to wait for them explicitly.
+	sigset_t signals;
+	sigemptyset(&signals);
+	sigaddset(&signals, SIGINT);
+	sigaddset(&signals, SIGQUIT);
+	sigaddset(&signals, SIGTERM);
+	sigprocmask(SIG_BLOCK, &signals, nullptr);
 
-	if (sigaction(SIGINT, &sa, NULL) < 0 ||
-	    sigaction(SIGQUIT, &sa, NULL) < 0 ||
-	    sigaction(SIGTERM, &sa, NULL) < 0) {
-		std::cerr << "Error: Couldn't register signal handlers" << std::endl;
-		return -1;
-	}
+	struct timespec wait_time{wait, 0};
 
 	try {
 		// create Throttle object
 		Throttle throttle(config_fn, pipe_fn);
 
 		// Run the feedback loop
-		while (!term) {
+		do
 			throttle();
-			sleep(wait);
-		}
+		while (sigtimedwait(&signals, nullptr, &wait_time) == -1);
 	}
 	catch (std::runtime_error ex) {
 		std::cerr << "Error: " <<  ex.what() << std::endl;
